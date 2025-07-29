52d4dcee0038b1350aac63c90a7e8b085f9ec4ef
Monster2408 <meoto2408@gmail.com>
2025-07-29 17:26:24 +0900

    update

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+setup(
+    name='DiscordPyAuxiliaryLib',
+    version='1.0.0',
+    packages=['DiscordPyAuxiliaryLib'],
+    install_requires=[
+        'discord.py>=1.7.3',
+        # Add other dependencies here
+    ],
+    author='hashimotok',
+    author_email='contact@hashimotok.dev',
+    url='https://github.com/Monster2408/discord-py-auxiliary-lib',
+    download_url='https://github.com/Monster2408/discord-py-auxiliary-lib',
+    python_requires=">=3.10.6",
+    description='A library to assist with Discord.py development',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Operating System :: OS Independent',
+    ],
+    keywords='discord.py auxiliary library',
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'discord-py-auxiliary-lib=DiscordPyAuxiliaryLib.__main__:main',
+        ],
+    },
+    project_urls={
+        'Documentation': 'https://discord-py-auxiliary-lib.readthedocs.io/',
+        'Source': 'https://github.com/Monster2408/discord-py-auxiliary-lib',
+        'Tracker': 'https://github.com/Monster2408/discord-py-auxiliary-lib/issues',
+    },
+    license='MIT',
+    license_files=('LICENSE',),
+    package_data={
+        'DiscordPyAuxiliaryLib': ['data/*.json', 'data/*.yaml'],
+    },
+    scripts=['scripts/discord_py_auxiliary_script.py'],
+)
